5434e90a7fff4019c913c2ca7b897df4a6133410
beuzf01 <derkvb2006@gmail.com>
2025-02-05 13:43:24 +0100

    `tried database connection establish`

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from fastapi import FastAPI
+import psycopg2
 app = FastAPI()
+
+try:
+    db_connection = psycopg2.connect(
+    database="game_recommender_db",
+    user="do_admin",
+    password="AVNS_Z8a5xRd6XwRSreQr7QI",
+    host="game-recommender-api-do-user-18910148-0.l.db.ondigitalocean.com",
+    port="25060"
+
+)
+    print("Database connection established")
+except Exception as e:
+    print("we ran into a problem: " + str(e))
+
+
 
 @app.get("/")
 def read_root():
