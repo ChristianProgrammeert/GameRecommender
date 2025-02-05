69d266f5505a5aeb760c8b743920e5c0de2df3e8
beuzf01 <derkvb2006@gmail.com>
2025-02-05 16:04:51 +0100

    `renamed some files `

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
-app = FastAPI()
+from algorithm import *
+
 
 @app.get("/")
 def read_root():
     return {"message": "Welkom bij GameRecommender"}
+@app.get("/get_data_from_db")
+def genre_computing():
+    return get_genres()
+def get_game_data_from_db():
+    return get_games()
