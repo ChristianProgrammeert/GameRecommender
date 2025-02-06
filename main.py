405fe49c53349eeaf1211e52d6356530b405e66e
Christian Hilgenkamp <christianhilgenkamp@gmail.com>
2025-02-06 13:56:22 +0100

    Finished database initialisation

--- main.py
+++ main.py
@@ -1,31 +1,7 @@
-from dotenv import load_dotenv
 from fastapi import FastAPI
-import psycopg2
-import dotenv
-import os
 app = FastAPI()
-load_dotenv()
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_USER = os.getenv('DB_USER')
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
-DB_PORT = os.getenv('DB_PORT')
-
-try:
-    db_connection = psycopg2.connect(
-    database=DB_NAME,
-    user=DB_USER,
-    password=DB_PASSWORD,
-    host=DB_HOST,
-    port= DB_PORT
-
-)
-    print("Database connection established")
-except Exception as e:
-    print("we ran into a problem: " + str(e))
-
 
 
 @app.get("/")
 def read_root():
-    return {"message": "Welkom bij GameRecommender"}
+    return {"message": "Hello World"}
