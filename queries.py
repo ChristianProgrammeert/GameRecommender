89be5a884fc027413e94bb98334b550e260a8eaa
beuzf01 <derkvb2006@gmail.com>
2025-02-06 14:49:37 +0100

    changed algorithm to SQLalchemy by merging feature-database onto feature algorithm

--- queries.py
+++ queries.py
@@ -1,22 +1,10 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI, Depends
 import os
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import Session
-from models import Base, Genre, Game
-from database import SessionLocal
+from algorithm import *
 
 app = FastAPI()
 
-# Dependency to get DB session
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@app.get("/genres")
 def get_genres(db: Session = Depends(get_db)):
     genres = db.query(Genre).all()
     return {"data": genres}
