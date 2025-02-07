3358b394516e727cea2e76c179e83e787fe3b305
beuzf01 <derkvb2006@gmail.com>
2025-02-07 11:29:17 +0100

    tried getting results from database

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
-from dotenv import load_dotenv
-from fastapi import FastAPI
-import psycopg2
-import dotenv
-import os
+from queries import *
+
 app = FastAPI()
-load_dotenv()
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_USER = os.getenv('DB_USER')
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
-DB_PORT = os.getenv('DB_PORT')
 
-try:
-    db_connection = psycopg2.connect(
-    database=DB_NAME,
-    user=DB_USER,
-    password=DB_PASSWORD,
-    host=DB_HOST,
-    port= DB_PORT
 
-)
-    print("Database connection established")
-except Exception as e:
-    print("we ran into a problem: " + str(e))
 
 
 
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+def read_root(db: Session = Depends(get_db)):
+    genres = get_genres(db)
+    games = get_games(db)
+    return {"message": "Hello World",
+            "genres": genres["data"],
+            "games": games["data"]}
