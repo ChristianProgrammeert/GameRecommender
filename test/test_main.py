bf981c6f33266037a202b0a599a872d4b25fe4e0
KittoKazu <ajmstaring@gmail.com>
2025-02-07 14:06:45 +0100

    Fixed case where workflow considered GameRecommender a package

--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,6 @@
-from GameRecommender.main import read_root
+from GameRecommender import main
 import unittest
 
 class MainTest(unittest.TestCase):
     def test_main(self):
-        self.assertEqual(read_root(), {"message": "Hello World"}, msg="Api output doesn't match expectations.")
+        self.assertEqual(main.read_root({"message": "Hello World"}, msg="Api output doesn't match expectations."))
