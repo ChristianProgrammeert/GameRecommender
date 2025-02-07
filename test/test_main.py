fe2a209c99e683e296c98ade4f03520e40261f7d
KittoKazu <ajmstaring@gmail.com>
2025-02-07 14:14:37 +0100

    Fixed issue where test_main couldn't build due to wrong syntax

--- test/test_main.py
+++ test/test_main.py
@@ -3,4 +3,4 @@ import unittest
 
 class MainTest(unittest.TestCase):
     def test_main(self):
-        self.assertEqual(main.read_root({"message": "Hello World"}, msg="Api output doesn't match expectations."))
+        self.assertEqual(main.read_root(),{"message": "Hello World"}, msg="Api output doesn't match expectations.")
