5bc75fc15d4ed0c11816c45425f914e76a11ec51
Christian Hilgenkamp <christianhilgenkamp@gmail.com>
2025-02-12 16:08:25 +0100

    Updated doctl

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,18 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.46.0"
+    }
+  }
+}
 provider "digitalocean" {
   token = var.do_token
+}
+
+resource "digitalocean_container_registry" "gr_registry" {
+  name                   = "game-recommender"
+  subscription_tier_slug = "starter"
 }
 
 resource "digitalocean_kubernetes_cluster" "gr_cluster" {
@@ -12,4 +25,8 @@ resource "digitalocean_kubernetes_cluster" "gr_cluster" {
     size       = "s-1vcpu-2gb"
     node_count = 1
   }
+}
+
+output "registry_endpoint" {
+  value = digitalocean_container_registry.gr_registry.endpoint
 }
