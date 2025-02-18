0189db94df9027ab29d5bb34fc14e0a5319943e4
KittoKazu <ajmstaring@gmail.com>
2025-02-18 16:22:49 +0100

    Took out headers in order to fix critical bug where api crashes when an exception is raised

--- app/input_parser.py
+++ app/input_parser.py
@@ -25,13 +25,13 @@ def check_characters(input_string):
         if _ in wanted_characters and input_string: #input_string returns true if the input string is not empty.
             continue
         else:
-            raise HTTPException(status_code = 400, detail = unexpected_characters_message, headers = {"Expected": f"Characters in {wanted_characters}"})
+            raise HTTPException(status_code = 400, detail = unexpected_characters_message,)
     return True
 
 def check_length(input_string):
     """Checks whether the input string is of expected length."""
     if len(input_string) != wanted_length:
-        raise HTTPException(status_code = 400, detail = unexpected_length_message, headers = {"Expected ": f"{wanted_length} characters"})
+        raise HTTPException(status_code = 400, detail = unexpected_length_message,)
     else:
         return True
 
