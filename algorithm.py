369f9c8421c08a174c1073506bd74a2e9df00e8f
KittoKazu <ajmstaring@gmail.com>
2025-02-19 15:48:16 +0100

    Games and Genres are now uppercase

--- algorithm.py
+++ algorithm.py
@@ -14,7 +14,6 @@ def compute_genres(genres,rage,skill_base,action_pack):
     result = []
     for  genre in genres:
         if genre.rage_inducing == rage and genre.skill_based == skill_base and genre.action_packed == action_pack:
-            genre.name = genre.name.lower()
             result.append(genre)
     return result
 
@@ -22,7 +21,6 @@ def compute_games(games,mature_themes,open_world_,multiplayer):
     result = []
     for game in games:
         if game.open_world == open_world_ and game.mature_themes == mature_themes and game.multiplayer == multiplayer: # and game.length_in_hours <= length
-            game.name = game.name.lower()
             result.append(game)
     return result
 
