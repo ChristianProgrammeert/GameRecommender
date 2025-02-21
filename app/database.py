11089e682938576a17d97eb66e7c210ea0fa0e8b
KittoKazu <ajmstaring@gmail.com>
2025-02-21 15:04:52 +0100

    Found ip issue

--- app/database.py
+++ app/database.py
@@ -21,42 +21,6 @@ DB_PORT = os.getenv('DB_PORT')
 # Connection timeout is set to 60 seconds because it kept timing out
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require&connect_timeout=60"
 
-# try:
-#     engine = create_engine(
-#     DATABASE_URL,
-#     poolclass=QueuePool,
-#     pool_size=10,  # Active connections in the pool
-#     max_overflow=20,  # Extra connections allowed beyond the pool size
-#     pool_timeout=30  # Wait time before throwing a timeout error
-#     )
-#
-#     MAX_RETRIES = 5
-#
-#     for attempt in range(MAX_RETRIES):
-#         try:
-#             with engine.connect() as conn:
-#                 print("Connected successfully!")
-#                 break  # Success, exit loop
-#         except OperationalError:
-#             print(f"Retrying connection attempt: ({attempt + 1}/{MAX_RETRIES})")
-#             time.sleep(2)  # Wait before retrying
-#     else:
-#         raise Exception("Database connection failed after multiple attempts")
-#
-#     if engine is not None:
-#         SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-#         Base.metadata.create_all(engine)
-#
-#         inspector = inspect(engine)
-#         tables = inspector.get_table_names()
-#         print("Existing Tables:", tables)
-#
-#
-#
-# except Exception as e:
-#     print("We ran into a problem: " + str(e))
-
-# Maak de engine buiten de try-except zodat het altijd beschikbaar is
 engine = create_engine(
     DATABASE_URL,
     poolclass=QueuePool,
@@ -66,11 +30,11 @@ engine = create_engine(
 )
 Base.metadata.create_all(engine)
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 inspector = inspect(engine)
 tables = inspector.get_table_names()
 print("Existing Tables:", tables)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
     """Dependency to get database session"""
@@ -79,4 +43,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
