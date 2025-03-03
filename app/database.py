b4190ec7aea6681feb683e6cc9e2bfe1662237a8
Christian Hilgenkamp <christianhilgenkamp@gmail.com>
2025-03-03 14:21:54 +0100

    Made connstring 1 env var

--- app/database.py
+++ app/database.py
@@ -12,14 +12,7 @@ from app.models import Base
 if not os.getenv("DB_USER"):
     load_dotenv()
 
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_USER = os.getenv('DB_USER')
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
-DB_PORT = os.getenv('DB_PORT')
-
-# Connection timeout is set to 60 seconds because it kept timing out
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require&connect_timeout=60"
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(
     DATABASE_URL,
