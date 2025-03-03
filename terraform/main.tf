966c1ea889c9dd248fc01462af22752c8e59b708
Christian Hilgenkamp <christianhilgenkamp@gmail.com>
2025-03-03 10:48:19 +0100

    Cluster init

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,130 @@
+# Providers instellen
 terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "2.46.0"
+      version = "~> 2.0"
     }
   }
 }
+
 provider "digitalocean" {
   token = var.do_token
 }
 
-resource "digitalocean_container_registry" "gr_registry" {
-  name                   = "game-recommender"
-  subscription_tier_slug = "starter"
+
+provider "kubernetes" {
+  host                   = digitalocean_kubernetes_cluster.gr_cluster.endpoint
+  token                  = digitalocean_kubernetes_cluster.gr_cluster.kube_config[0].token
+  cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.gr_cluster.kube_config[0].cluster_ca_certificate)
 }
 
+# Kubernetes-cluster aanmaken
 resource "digitalocean_kubernetes_cluster" "gr_cluster" {
-  name    = "my-k8s-cluster"
-  region  = "ams3"
-  version = "1.28.0-do.0"
+  name    = var.cluster_name
+  region  = var.region
+  version = "1.32.1-do.0"
 
   node_pool {
-    name       = "default-node-pool"
-    size       = "s-1vcpu-2gb"
-    node_count = 1
+    name       = "gameapi"
+    size       = var.node_size
+    node_count = var.node_count
   }
 }
 
-output "registry_endpoint" {
-  value = digitalocean_container_registry.gr_registry.endpoint
+# Namespaces
+resource "kubernetes_namespace" "app_namespace" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+resource "kubernetes_secret" "docker_registry" {
+  metadata {
+    name      = "docker-registry"
+    namespace = kubernetes_namespace.app_namespace.metadata[0].name
+  }
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "registry.digitalocean.com" = {
+          username = var.docker_username
+          password = var.do_token
+          auth     = base64encode("${var.docker_username}:${var.do_token}")
+        }
+      }
+    })
+  }
+}
+
+# Website Deployment binnen het Kubernetes-cluster
+resource "kubernetes_deployment" "website" {
+  metadata {
+    name      = "website"
+    namespace = var.namespace
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "website"
+      }
+    }
+
+    strategy {
+      type = "RollingUpdate"
+      rolling_update {
+        max_unavailable = 0
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "website"
+        }
+      }
+      spec {
+        image_pull_secrets {
+          name = kubernetes_secret.docker_registry.metadata[0].name
+        }
+        container {
+          name  = "website"
+          image = "registry.digitalocean.com/game-recommender/gameapi:latest"
+          image_pull_policy = "Always"
+          port {
+            container_port = 8000
+          }
+        }
+          image_pull_secrets {
+          name = "docker_registry"
+        }
+      }
+
+    }
+  }
+}
+
+
+resource "kubernetes_service" "gameapi-loadbalancer" {
+  metadata {
+    name      = "gameapi-loadbalancer"
+    namespace = kubernetes_namespace.app_namespace.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "website" # Match labels van je deployment
+    }
+
+    port {
+      protocol    = "TCP"
+      port        = 80       # Externe poort
+      target_port = var.container_port    # Poort waar je app luistert
+    }
+
+    type = "LoadBalancer"
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,49 @@ variable "do_token" {
   type        = string
   description = "DigitalOcean API Token"
 }
+
+variable "cluster_name" {
+  type        = string
+  description = "Name of the Kubernetes cluster"
+}
+
+variable "region" {
+  type        = string
+  description = "Region where the Kubernetes cluster will be created"
+}
+
+variable "node_size" {
+  type        = string
+  description = "Size of the nodes in the Kubernetes cluster"
+}
+
+variable "namespace" {
+  type        = string
+  description = "Namespace where the application will be deployed"
+}
+
+variable "monitoring_namespace" {
+  type        = string
+  description = "Namespace where monitoring resources will be deployed"
+}
+
+variable "container_port" {
+  type        = number
+  description = "Port where the application will be exposed"
+}
+
+variable "node_count" {
+  type        = number
+  description = "Number of nodes in the Kubernetes cluster"
+}
+
+variable "docker_username" {
+  type        = string
+  description = "Docker username for the Docker registry"
+}
+
+variable "website_image" {
+  type        = string
+  description = "Docker image for the website deployment"
+}
+
