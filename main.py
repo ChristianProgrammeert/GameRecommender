4dce1cbd6ce5dc9cdec2f258b6c3c957d9c20b31
KittoKazu <ajmstaring@gmail.com>
2025-03-12 16:20:23 +0100

    Cleaned up code in main

--- main.py
+++ main.py
@@ -18,16 +18,8 @@ def boolean_validation_exception_handler(request: Request, err: RequestValidatio
     raise err
 @app.get("/recommendation")
 def endpoint_recommender(rage_inducing:bool = None,action_packed:bool = None, skill_based:bool = None,mature_themes:bool = None,open_world:bool = None,multiplayer:bool = None,db: Session = Depends(get_db)):
-    input_list = [
-        rage_inducing,
-        action_packed,
-        skill_based,
-        mature_themes,
-        open_world,
-        multiplayer
-    ]
-
-    AnswerClass = parser.parse_input(input_list)
+    AnswerClass = parser.parse_input([bool_value for var_name, bool_value in locals().items() if var_name != 'db'])
+    # Makes a list from all the input booleans, excluding the db variable.
     return {"Recommendations":algorithm.link_games_genres(
         algorithm.compute_games(queries.get_games(db), AnswerClass.is_mature, AnswerClass.is_open_world, AnswerClass.is_multiplayer),
         algorithm.compute_genres(queries.get_genres(db), AnswerClass.is_rage_inducing, AnswerClass.is_action_packed, AnswerClass.is_skill_based),
