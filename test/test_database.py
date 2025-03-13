eed53c9d85acc3957bdda110c5fa59177e349ba5
Christian Hilgenkamp <christianhilgenkamp@gmail.com>
2025-03-13 14:19:44 +0100

    Changed test db if

--- test/test_database.py
+++ test/test_database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import QueuePool
 
 
-if not os.getenv("DB_USER"):
+if not os.getenv("TEST_DATABASE_URL"):
     load_dotenv()
 
 TEST_DATABASE_URL = os.getenv('TEST_DATABASE_URL')
